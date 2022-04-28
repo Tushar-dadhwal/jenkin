fee4f5a3115149cfada457396388a713257a015e
Tushar-dadhwal <54946881+Tushar-dadhwal@users.noreply.github.com>
2022-04-28 16:11:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello World!!")
-print("From tushar")
+print("Names of all courses in this semester:")
+print("1. HPC - 4 \n2. DevOps - 2 \n3. SPM - 2 \n4. EVS - 1 \n5. Career Skills - 3 \n6. Communication Skills - 2 \n7. Seminar on Special Topics - 1 \n8. Capstone Design - 2 \n9. Undergraduate Research - 2")
+
+Names of all courses in this semester:
+1. HPC - 4 
+2. DevOps - 2 
+3. SPM - 2 
+4. EVS - 1 
+5. Career Skills - 3 
+6. Communication Skills - 2 
+7. Seminar on Special Topics - 1 
+8. Capstone Design - 2 
+9. Undergraduate Research - 2
+
