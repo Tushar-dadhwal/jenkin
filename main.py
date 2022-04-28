720243801b0c68bd1d2ddf1bc02ac072287631d6
Tushar-dadhwal <54946881+Tushar-dadhwal@users.noreply.github.com>
2022-04-28 16:32:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 print("Names of all courses in this semester:")
-print("1. HPC - 4 \n2. DevOps - 2 \n3. SPM - 2 \n4. EVS - 1 \n5. Career Skills - 3 \n6. Communication Skills - 2 \n7. Seminar on Special Topics - 1 \n8. Capstone Design - 2 \n9. Undergraduate Research - 2")
-
-Names of all courses in this semester:
-1. HPC - 4 
-2. DevOps - 2 
-3. SPM - 2 
-4. EVS - 1 
-5. Career Skills - 3 
-6. Communication Skills - 2 
-7. Seminar on Special Topics - 1 
-8. Capstone Design - 2 
-9. Undergraduate Research - 2
-
+print("1. HPC - 4")
+print ("2 . DevOps - 2")
+print("3 . SPM - 2 ")
+print ("4. EVS - 1")
+print ("5. Career Skills - 3")
